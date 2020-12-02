347c2ed38ef8247fd8f6422f21200669541e9ef3
James Hernandez <james@j-hernandez.com>
2020-12-01 20:28:50 -0500

    initial commit

---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id serial primary key,
+    name varchar(50) not null,
+    email varchar(50) NoT null
+);
---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users
+    (name, email)
+VALUES
+    ('Alice', 'alice@gmail.com'),
+    ('James', 'james@test.com'),
+    ('Donald', 'donald@yahoo.com'), 
+    ('Test', 'test@test.com');
